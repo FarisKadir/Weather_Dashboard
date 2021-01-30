76d2212cffa198ed7c729e58af51ab38620c7f3f
FarisKadir <faris.kadir@gmail.com>
2021-01-29 22:14:29 -0500

    Completed the ajax call. Updated the html file so that I could display some of the data from the response.

--- script.js
+++ script.js
@@ -5,15 +5,33 @@ $(document).ready(function()    {
     $(".searchBtn").on("click", function()  {
         var city = $("#search").val();
         var key = $("#key").val();
-        console.log(city);
-        queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
-        console.log(queryURL);
+        
+        queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
+        
         //API Call
         $.ajax({
             url: queryURL,
             method: "GET"
           }).then(function(response) {
-            console.log(response.coord.lon);
+              
+              var temp = ((response.main.temp - 273.15) * 9/5 +32).toFixed(2);
+              
+              var city = response.name;
+
+              var country = response.sys.country;
+              
+              var humidity = response.main.humidity;
+
+              var wind = response.wind.speed;
+
+              var uvInd = response.
+              
+
+            $("#citydate").text(city.toString() + ", " + country + "  (" + moment().format("MM/DD/YYYY") + ")");
+            $("#temp").text("Temperature: " + temp.toString() + " F");
+            $("#humidity").text("Humidity: " + humidity.toString());
+            $("#wind").text("Wind: " + wind.toString());
+            $("#uv").text("UV Index: ");
         });
     });
 
