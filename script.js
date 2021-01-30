76a9af624fca5f67a1125461914f0091f3026db4
FarisKadir <faris.kadir@gmail.com>
2021-01-30 16:39:18 -0500

    Updated the function to get the forecast.

--- script.js
+++ script.js
@@ -1,55 +1,40 @@
 $(document).ready(function()    {
-
     var key = "60e01cf8819d6fe551736d0879242403";
-    var lon = "";
-    var lat = "";
+    var coord = [];
+    
 
-    function getCoord() {
+    //Function for retrieving the latitude and longitude
+    function getForecast(city) {
+        var forecastURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
         $.ajax({
-            url:forecastURL,
+            url: forecastURL,
             method: "GET"
         }) .then(function(response) {
-            lon = response.coord.lon;
-            lat = response.coord.lat;
-            return;
-        })
-    }
-
+            var oneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + key;
+            var country = response.sys.country;
+            $.ajax({
+                url: oneCallURL,
+                method: "GET"
+            }) .then(function(response) {
+                    temp = response.current.temp;
+                    humidity = response.current.humidity;
+                    wind = response.current.wind_speed;
+                    uvInd = response.current.uvi;
+                    $("#citydate").text(city.charAt(0).toUpperCase() + city.slice(1) + ", " + country + "  (" + moment().format("MM/DD/YYYY") + ")");
+                    $("#temp").text("Temperature: " + temp.toString() + " F");
+                    $("#humidity").text("Humidity: " + humidity.toString());
+                    $("#wind").text("Wind: " + wind.toString());
+                    $("#uv").text("UV Index: " + uvInd); 
+            })
+        });
+    };
 
 
     $(".searchBtn").on("click", function()  {
-        var city = $("#search").val();
-        
-        
-        queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
-        console.log(queryURL);
-        
-        //API Call to get the current forecast
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).then(function(response) {
-              
-              var temp = ((response.main.temp - 273.15) * 9/5 +32).toFixed(2);
-              
-              var city = response.name;
-
-              var country = response.sys.country;
-              
-              var humidity = response.main.humidity;
-
-              var wind = response.wind.speed;
-
-              var uvInd = '';
-
-            $("#citydate").text(city.toString() + ", " + country + "  (" + moment().format("MM/DD/YYYY") + ")");
-            $("#temp").text("Temperature: " + temp.toString() + " F");
-            $("#humidity").text("Humidity: " + humidity.toString());
-            $("#wind").text("Wind: " + wind.toString());
-            $("#uv").text("UV Index: ");
-        });
+        getForecast($("#search").val());
     });
 
 
 
-}) //End of document ready   
+
+}); //End of document ready   
